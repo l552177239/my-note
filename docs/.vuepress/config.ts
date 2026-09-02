a1af0b2025792e242667a3fc44aecc1ebdd55f9d
l552177239 <552177239@qq.com>
2026-09-02 20:17:32 +0800

    Enhance documentation: Add new Node.js and Linux tutorials, update existing content, and improve structure across multiple files. Include new sections on Express and MongoDB, and refine Linux command line and file operations documentation.

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -108,6 +108,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
           },
           { text: 'MySQL', link: '/pages/mysql-basic/', items: [] },
           { text: 'Redis', link: '/pages/redis-basic/', items: [] },
+          { text: 'Nodejs', link: '/pages/fdb-12b2a966a0/' },
         ],
       },
       {
@@ -115,6 +116,8 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
         link: '/technology/',
         items: [
           { text: '技术文档', link: '/pages/9a7ee40fc232253e/' },
+          { text: 'Linux', link: '/pages/fdb-3187a9667b/' },
+          { text: 'Git', link: '/pages/fdb-7a77510d52/' },
           { text: 'GitHub技巧', link: '/pages/4c778760be26d8b3/' },
           { text: 'Nodejs', link: '/pages/117708e0af7f0bd9/' },
           { text: '博客搭建', link: '/pages/41f87d890d0a02af/' },
